835b5f019cd62fd1aecb1b105596c62f06bcc7e1
azzurragraziano <azzurragraziano02@email.com>
2022-06-23 15:59:29 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+BONUS:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data (in data avrò l'src). */
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            title: 'vue hello!'
+        }
+    }
+);
